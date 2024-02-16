7aea105b9b056f658b866e4df58ad170c5538282
jmschrei <jmschreiber91@gmail.com>
2024-02-16 07:13:25 +0000

    RM nose requirement

--- tests/toy_models.py
+++ tests/toy_models.py
@@ -2,18 +2,9 @@
 # Contact: Jacob Schreiber <jmschreiber91@gmail.com>
 
 import torch
-import pytest
-
-from tangermeme.utils import one_hot_encode
-from tangermeme.utils import random_one_hot
-
-from nose.tools import assert_raises
-from numpy.testing import assert_array_almost_equal
-
 
 torch.manual_seed(0)
 torch.use_deterministic_algorithms(True)
-
 
 class SumModel(torch.nn.Module):
 	def __init__(self):
