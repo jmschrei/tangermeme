0691c229a9b6825e5a73fe890bac0db38f041e48
Jacob Schreiber <jmschreiber91@gmail.com>
2024-05-07 17:12:54 +0000

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tangermeme',
-    version='0.1.0',
+    version='0.2.0',
     author='Jacob Schreiber',
     author_email='jmschreiber91@gmail.com',
     packages=['tangermeme', 'tangermeme/tools'],
